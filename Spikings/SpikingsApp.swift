1f1621a58019a1b2a90dbcaf93850f20f7a8dc82
Maksim Kosharny <maksimkosharnyjj@gmail.com>
2026-01-28 14:47:47 +0800

    Initial Commit

---
+++ Spikings/SpikingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpikingsApp.swift
+//  Spikings
+//
+//  Created by Maksim Kosharny on 28.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct SpikingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
